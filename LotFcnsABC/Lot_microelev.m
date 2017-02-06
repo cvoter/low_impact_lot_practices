9e071141af786ed25203cdb1c63ffd785f793e7d
Carolyn Voter <cvoter@wisc.edu>
2017-02-06 11:51:28 -0600

    Made approach specific to AWRA 2017 lots

--- LotFcnsABC/Lot_microelev.m
+++ LotFcnsABC/Lot_microelev.m
@@ -1,4 +1,4 @@
-function [] = Lot_microelev(nx,ny,sd,landslope,lotname)
+function [] = Lot_microelev(sd,landslope,lotname)
 %LotB_elevation.m
 %Carolyn Voter
 %October 18, 2016
@@ -9,10 +9,17 @@ function [] = Lot_microelev(nx,ny,sd,landslope,lotname)
 % To account for pit-filling later on, do:
 % RR = 3.75cm --> DS = 7.7mm --> DS ~= 5mm after pit filling
 
-%Example command:
+%Example command (orig):
 %Lot_microelev(48,88,0.0285,0.02,'LotA');
 %Lot_microelev(27,84,0.0285,0.02,'LotB');
 %Lot_microelev(32,84,0.0285,0.02,'LotC');
+
+%Example command (new, load AWRA data first)
+% Lot_microelev(0.0375,0.02,'AWRA_15');
+
+%% Load AWRA data
+AWRAdir = 'J:\Research\Subprojects\ResidentialLayouts\Madison\parflow';
+load(strcat(AWRAdir,'\',lotname,'S.mat'));
 
 %% Generate random roughness
 for i = 1:ny+1
@@ -31,6 +38,6 @@ end
 RRcalc = sqrt((1/(ny*nx-1))*RRinc)*100; %Random roughness [cm]
 DScalc = 0.112*RRcalc+0.031*RRcalc^2-0.012*RRcalc*landslope*100; %Depression storage [cm]
 
-savename=strcat(lotname,'_microelev.mat');
+savename=strcat(AWRAdir,'\',lotname,'_microelev.mat');
 save(savename,'microElev','sd','RRcalc','DScalc');
 end
